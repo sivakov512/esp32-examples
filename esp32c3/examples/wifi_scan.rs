9d0ec135fdf3c7c1ea4aa1e577007aadd3b9f8a5
Nikita Sivakov <sivakov512@icloud.com>
2024-05-11 23:02:11 +0200

    WiFi scan example

---
+++ esp32c3/examples/wifi_scan.rs
@@ -0,0 +1,49 @@
+#![no_std]
+#![no_main]
+
+use esp_backtrace as _;
+use esp_hal::{
+    clock::ClockControl, peripherals::Peripherals, prelude::*, rng::Rng, systimer::SystemTimer,
+};
+use esp_println::println;
+use esp_wifi::wifi;
+use smoltcp::iface::SocketStorage;
+
+#[entry]
+fn main() -> ! {
+    let peripherals = Peripherals::take();
+    let system = peripherals.SYSTEM.split();
+
+    let clocks = ClockControl::max(system.clock_control).freeze();
+
+    let timer = SystemTimer::new(peripherals.SYSTIMER).alarm0;
+    let wifi_init = esp_wifi::initialize(
+        esp_wifi::EspWifiInitFor::Wifi,
+        timer,
+        Rng::new(peripherals.RNG),
+        system.radio_clock_control,
+        &clocks,
+    )
+    .unwrap();
+
+    let mut socket_set_entries: [SocketStorage; 3] = Default::default();
+    let (_, _, mut controller, _) = wifi::utils::create_network_interface(
+        &wifi_init,
+        peripherals.WIFI,
+        wifi::WifiStaDevice,
+        &mut socket_set_entries,
+    )
+    .unwrap();
+
+    controller.start().unwrap();
+
+    let res: Result<(heapless::Vec<wifi::AccessPointInfo, 10>, usize), wifi::WifiError> =
+        controller.scan_n();
+    if let Ok((res, _count)) = res {
+        for ap in res {
+            println!("{:?}", ap);
+        }
+    }
+
+    panic!()
+}
